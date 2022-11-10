ab57b2b198453ebd552e06352b02b4b55efd99c7
msiller455 <msiller455@gmail.com>
2022-11-10 13:27:55 -0800

    adds SignUpForm

---
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react'
+
+export default class SignUpForm extends Component {
+
+    render() {
+        return(
+            <div>
+                SignUpForm
+            </div>
+        )
+    }
+}
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,17 +4,21 @@ import './App.css';
 import AuthPage from '../AuthPage/AuthPage'
 import NewOrderPage from '../NewOrderPage/NewOrderPage'
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage'
+import NavBar from '../../components/NavBar/NavBar'
 
 export default function App() {
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState(null)
 
   return (
     <main className="App">
       { user ? 
-        <Routes>
-          <Route path="/orders/new" element={<NewOrderPage />} />
-          <Route path="/orders" element={<OrderHistoryPage />} />
-        </Routes>
+        <>
+          <NavBar />
+          <Routes>
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders" element={<OrderHistoryPage />} />
+          </Routes>
+        </>
         :
         <AuthPage />
       }
