256a13cd99bacb1d38737e0c13ac0fbbe927fca1
msiller455 <msiller455@gmail.com>
2022-11-10 15:02:52 -0800

    adds SignUpForm function component

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,1 +1,45 @@
 // Rewrite the SignUpForm as a function component
+import { useState } from 'react'
+
+export default function SignUpForm() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirm: '',
+        error: ''
+    })
+    const disable = formData.password !== formData.confirm;
+
+    function handleChange(evt) {
+        setFormData({
+            ...formData,
+            [evt.target.name]: evt.target.value,
+            error: ''
+        })
+    }
+
+    function handleSubmit(evt) {
+        evt.preventDefault()
+        alert(JSON.stringify(formData))
+    }
+
+    return(
+        <div>
+            <div className="form-container">
+              <form autoComplete="off" onSubmit={handleSubmit}>
+                <label>Name</label>
+                <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+                <label>Email</label>
+                <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+                <label>Password</label>
+                <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+                <label>Confirm</label>
+                <input type="password" name="confirm" value={formData.confirm} onChange={handleChange} required />
+                <button type="submit" disabled={disable}>SIGN UP</button>
+              </form>
+            </div>
+            <p className="error-message">&nbsp;{formData.error}</p>
+          </div>
+    )
+}
