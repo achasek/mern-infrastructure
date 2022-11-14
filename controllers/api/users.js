bd6310a9fec8047f6ac4d1a9e94f0b09bcd1d2f2
msiller455 <msiller455@gmail.com>
2022-11-14 14:25:48 -0800

    adds User model and returns JWT to client

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,12 +1,26 @@
+const User = require('../../models/user')
+const jwt = require('jsonwebtoken')
+
 module.exports = {
     create,
 }
 
-function create(req, res) {
-    res.json({
-        user: {
-            name: req.body.name,
-            email: req.body.email
-        }
-    })
+async function create(req, res) {
+    try {
+        const user = await User.create(req.body)
+        const token = createJWT(user)
+        res.json(token)
+    } catch(err) {
+        res.status(400).json(err)
+    }
+}
+
+
+/*-- Helper Functions --*/
+function createJWT(user) {
+    return jwt.sign(
+        { user },
+        process.env.SECRET,
+        { expiresIn: '24h' }
+    )
 }
