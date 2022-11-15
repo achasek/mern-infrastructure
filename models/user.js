3535594b6f6b369031fcfd3949bae84f73e30d39
msiller455 <msiller455@gmail.com>
2022-11-15 10:08:35 -0800

    adds service functions for retrieving token and user

--- models/user.js
+++ models/user.js
@@ -35,5 +35,4 @@ userSchema.pre('save', async function(next) {
     this.password = await bcrypt.hash(this.password, SALT_ROUNDS)
     return next()
 })
-
 module.exports = mongoose.model('User', userSchema)
--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -4,6 +4,22 @@ export async function signUp(userData) {
     // Delegate the network request code to the users-api.js API module
     // which will ultimately return a JSON Web Token (JWT)
     const token = await usersAPI.signUp(userData)
-    // Baby step by returning whatever is sent back by the server
+    localStorage.setItem('token', token)
     return token
+}
+
+export function getToken() {
+    const token = localStorage.getItem('token')
+    if(!token) return null
+    const payload = JSON.parse(atob(token.split('.')[1]))
+    if (payload.exp < Date.now() / 1000) {
+        localStorage.removeItem('token')
+        return null
+    }
+    return token
+}
+
+export function getUser() {
+    const token = getToken()
+    return token ? JSON.parse(atob(token.split('.')[1])).user : null
 }
